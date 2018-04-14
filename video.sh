075ae9552edd86d01ceb6430f429f55323f5b06d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-14 18:34:47 +0000

    Some updates
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- video.sh
+++ video.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DOUBLE=1 DIRECTION="0.93;0.79;3000" ./poleshift world.jpg 0 0 1080 540 video/f.jpeg
-ffmpeg -framerate 25 -pattern_type glob -i 'video/*.jpeg' -c:v libx264 -r 25 -pix_fmt yuv420p video.mp4
+DOUBLE=1 DIRECTION="0.93;0.79;6000" ./poleshift world.jpg 0 0 1920 1080 video/f.jpeg
+ffmpeg -framerate 20 -pattern_type glob -i 'video/*.jpeg' -c:v libx264 -r 20 -pix_fmt yuv420p video.mp4
